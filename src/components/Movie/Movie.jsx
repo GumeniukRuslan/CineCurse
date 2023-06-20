aba682adc72502eaa9ce0807dee752ddabea6cf5
GumeniukRuslan <bishob1997@gmail.com>
2023-06-20 12:17:16 +0300

    minor

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -13,7 +13,7 @@ export const Movie = ({movie, backLink}) => {
         <Test>
           <StyledTitle>{movie.title}</StyledTitle>
           <Description>{movie.overview}</Description>
-          {movie.vote_count ? <Chip>{movie.vote_count ? movie.vote_average.toFixed(1) : ''}</Chip> : <></>}
+          {movie.vote_count ? <Chip type={movie.vote_average}>{movie.vote_count ? movie.vote_average.toFixed(1) : ''}</Chip> : <></>}
           
           <AditionalInfo>
             <AditionalInfoLink><Link style={{color: 'white', fontSize: '20px'}} to='cast'>Cast</Link></AditionalInfoLink>
