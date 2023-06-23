28f6c55afbbf7a9b1e95ef6a8c9c863000dc424b
GumeniukRuslan <bishob1997@gmail.com>
2023-06-23 17:26:00 +0300

    Update MovieItem.jsx

--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -12,7 +12,7 @@ export const MovieItem = ({ movie, path }) => {
       <Link to={`/movies/${movie.id}`} state={path}>
         <ImageStyled loading="lazy" src={movie.poster_path ? `${DEFAULT_VALUES.IMG_PATH}${movie.poster_path}` : DEFAULT_VALUES.POSTER} alt={movie.title} />
         <MovieTitle>{movie.title}</MovieTitle>
-        {movie.vote_count ? <Chip type={ movie.vote_average}>{movie.vote_average.toFixed(1)}</Chip> : released ? <ComingSoon>Coming soon</ComingSoon> : <ComingSoon>Reviews missing</ComingSoon>}
+        {movie.vote_count ? <Chip type={ movie.vote_average}>{movie.vote_average.toFixed(1)}</Chip> : released ? <ComingSoon>Coming soon</ComingSoon> : <ComingSoon>No reviews</ComingSoon>}
       </Link>
     </MovieItemStyled>
   )
