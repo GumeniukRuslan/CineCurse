e1cd6e741f92519b0a4e3e39251d38ab01a5cdb8
GumeniukRuslan <bishob1997@gmail.com>
2023-06-23 17:37:49 +0300

    Update Movie.jsx

--- src/components/Movie/Movie.jsx
+++ src/components/Movie/Movie.jsx
@@ -1,6 +1,6 @@
 
 import { AditionalInfo, AditionalInfoItem, BackLinkBtn, Description, FlexContainer, MoviePoster, PosterWrapper, ReleaseDate, StyledNavLink, StyledTitle } from "./movie.styled"
-import { Chip, ComingSoon } from "components/MovieItem/movieItem.styled"
+import { Chip, NoReviews } from "components/MovieItem/movieItem.styled"
 import PropTypes from 'prop-types';
 import { DEFAULT_VALUES } from "constants/constants";
 import { StyledDate } from "components/ReviewItem/reviewItem.styled";
@@ -24,7 +24,7 @@ export const Movie = ({ movie, backLink }) => {
         <div>
           <StyledTitle>{movie.title}</StyledTitle>
           <Description>{movie.overview ? movie.overview : DEFAULT_VALUES.DESCRIPTION}</Description>
-          {movie.vote_count ? <Chip type={ movie.vote_average}>{movie.vote_average.toFixed(1)}</Chip> : released ? <ComingSoon>Coming soon</ComingSoon> : <ComingSoon>Reviews missing</ComingSoon>}
+          {movie.vote_count ? <Chip type={ movie.vote_average}>{movie.vote_average.toFixed(1)}</Chip> : <NoReviews>{released ? 'Coming soon' : 'No reviews'}</NoReviews>}
           
           <AditionalInfo>
             <AditionalInfoItem><StyledNavLink to='cast'>Cast</StyledNavLink></AditionalInfoItem>
